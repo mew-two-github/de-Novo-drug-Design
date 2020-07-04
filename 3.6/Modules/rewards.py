596b566b5b85a24585d935422a7a7fdc88282363
vishal <subhavishal@yahoo.co.in>
2020-07-04 20:16:29 +0530

    added xgboost stuff

--- 3.6/Modules/rewards.py
+++ 3.6/Modules/rewards.py
@@ -92,47 +92,52 @@ def bunch_evaluation(mols):
          except:
              SSSR.append(False)
          i = i +1
+
     get_padel(folder_path,file_path)
 
-      #Reading the descriptors
-    X = pd.read_csv(file_path)
-    #Filling Null Values
-    X.fillna(value=0,inplace=True)
-    X.Name = pd.to_numeric(X.Name, errors='coerce')
-    X.sort_values(by='Name',inplace=True)
-    #X.to_csv('./try.csv',index=False)
-    #Removing the columns with zero variance in original data
-    with open('./saved_models/drop.txt','rb') as fp:
-        bad_cols = pickle.load(fp)
-    X_step1 = X.drop(columns=bad_cols,inplace=False)
-    X_step2 = X_step1
+    #Reading the descriptors
+    xg_all = pd.read_csv(file_path)
+
+    names = xg_all['Name']
+
+    bad = []
+    with open('./saved_models/good_columns','rb') as f:
+        cols = pkl.load(f)
+    for col in xg_all.columns:
+        if col not in cols:
+            bad.append(col)
+    xg_all.drop(columns=bad,inplace=True)
+    #Verifying that all the required columns are there
+    assert len(xg_all.columns) == len(cols)
+    xg_all['Name'] = names
+
+    files = xg_all[pd.isnull(xg_all).any(axis=1)]['Name']
+    xg_all.dropna(inplace=True)
+    mol= []
+    for f in files:
+        m = Chem.MolFromMolFile(folder_path+str(f)+'.mol')
+        mol.append(m)
+
+    i = 0
+    for m in mol:
+        print(Chem.MolToMolBlock((m)),file=open(str(folder_path)+str(files[i])+'.mol','w'))
+        i = i + 1
+    get_padel(folder_path,'./uneval_desc.csv','-1')
+    unevalmol = pd.read_csv('./uneval_desc.csv')
+
+
+    unevalmol.drop(columns=bad,inplace=True)
+    print(unevalmol.isna().sum(axis=1))
+
+    regressor = xgb.XGBRegressor()
+    regressor.load_model('./saved_models/best_from_gs38.model')
+
+    xg_all_all = pd.concat([xg_all,unevalmol])
+    xg_all_all.sort_values(by='Name',inplace=True)
+    xg_all_all.drop(columns='Name',inplace=True)
+    preds = regressor.predict(xg_all_all)
     
-    
-    
-    #Doing StandardScaler() as applied to original data
-    with open('./saved_models/new_scaler.pkl','rb') as fp:
-        scaler = pickle.load(fp)
-    X2 = scaler.transform(X_step2.astype('float64'))
-    X_step3 = pd.DataFrame(data=X2,columns=X_step2.columns)
-    
-    #X.head()
-    #Dropping columns with low correlation with pIC50
-    
-    # =============================================================================
-    #     X.to_csv('./X.csv',index=False)
-    #     X_step1.to_csv('./X_step1.csv')
-    #     X_step2.to_csv('./X_step2.csv')
-    X_step3.to_csv('./X_step3.csv')
-    # =============================================================================
-    
-    
-    
-    #Using the Random forest Predictor
-    with open('./saved_models/new_RFR.pkl','rb') as fp:
-        pp = pickle.load(fp)
-    predictions = pp.predict(X_step3)
-    
-    print('Properties predicted for {} molecules'.format(len(predictions)))
+    print('Properties predicted for {} molecules'.format(len(preds)))
     
     Evaluations = []
     j  = 0
